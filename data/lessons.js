ef0d1e2f8ee666efdc93db9468d5d1d08d9bfbf4
mariewoschke-lang <marie.woschke@gmail.com>
2026-07-06 10:28:32 +0200

    Create lessons.js

---
+++ data/lessons.js
@@ -0,0 +1,31 @@
+const lessons = [
+
+{
+    id:1,
+
+    world:1,
+
+    chapter:1,
+
+    title:"안녕하세요",
+
+    pronunciation:"Annyeonghaseyo",
+
+    translation:"Hallo",
+
+    explanation:"안녕하세요 ist die höfliche Begrüßung. Sie wird im Alltag sehr häufig verwendet.",
+
+    question:"Was bedeutet 안녕하세요?",
+
+    answers:[
+        "Hallo",
+        "Danke",
+        "Schule"
+    ],
+
+    correct:0,
+
+    xp:10
+}
+
+];
